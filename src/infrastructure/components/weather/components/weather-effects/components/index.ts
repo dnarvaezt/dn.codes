f61ecd34ebc9b7af012ff86faddafdbf72f125d0
David Narvaez <fnarvaez@ubits.co>
2025-10-18 01:04:54 -0500

    feat: add LightningEffect component to enhance weather effects
    - Introduced LightningEffect component to simulate lightning during specific weather conditions.
    - Updated WeatherEffects component to include LightningEffect for improved visual representation.
    - Added styles for LightningEffect to manage animations and visual effects.
    - Exported LightningEffect from the components index for easy access.

--- src/infrastructure/components/weather/components/weather-effects/components/index.ts
+++ src/infrastructure/components/weather/components/weather-effects/components/index.ts
@@ -1,3 +1,4 @@
+export * from "./lightning-effect"
 export * from "./star-field"
 export * from "./weather-background"
 export * from "./weather-clouds"
--- src/infrastructure/components/weather/components/weather-effects/weather-effects.tsx
+++ src/infrastructure/components/weather/components/weather-effects/weather-effects.tsx
@@ -1,12 +1,13 @@
 "use client"
 
-import { StarField, WeatherBackground, WeatherClouds } from "./components"
+import { LightningEffect, StarField, WeatherBackground, WeatherClouds } from "./components"
 import "./weather-effects.scss"
 
 export const WeatherEffects = () => {
   return (
     <div className={"weather-effects"}>
       <WeatherClouds />
+      <LightningEffect />
       <StarField />
       <WeatherBackground />
     </div>
